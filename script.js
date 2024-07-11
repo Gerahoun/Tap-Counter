e5db8d9a8306a3c6df18e5a62351fb2eeba2493d
Gerahoun <gerahoun@gmail.com>
2024-07-11 23:17:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const coin = document.querySelector('.coin');
 
     coin.addEventListener('click', function() {
-        coin.style.transform = 'translate(-50%, -50%) scale(0.9)';
+        coin.style.transform = 'translate(-50%, -50%) scale(0.95)';
 
         setTimeout(function() {
             coin.style.transform = 'translate(-50%, -50%) scale(1)';
